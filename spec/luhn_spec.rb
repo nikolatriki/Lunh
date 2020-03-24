f133812cd133125604027de25175a5434a31f6b0
nikolatriki <kjofte@yahoo.com>
2020-03-24 22:31:30 +0100

    added tests

--- spec/luhn_spec.rb
+++ spec/luhn_spec.rb
@@ -26,12 +26,62 @@ RSpec.describe Tools::Luhn do
 
   it 'becomes invalid if string reversed' do
     my_string = Tools::Luhn.new('59')
-    expect(my_string.reverse_valid?).to be false
+    expect(my_string.valid?).to be true
   end
 
   it 'valid Canadian sin' do
-    my_string = Tools::Luhn.new('055444285')
-    expect(my_string.luhn).to eq(my_string.luhn % 10)
+    my_string = Tools::Luhn.new('055 444 285')
+    expect(my_string.valid?).to be true
+  end
+
+  it 'invalid Canadian sin' do
+    my_string = Tools::Luhn.new('055 444 286')
+    expect(my_string.valid?).to be false
+  end
+
+  it 'invalid credit card' do
+    my_string = Tools::Luhn.new('827 1232 7352 0569')
+    expect(my_string.valid?).to be false
+  end
+
+  it 'valid string with a non digit included become invalid' do
+    my_string = Tools::Luhn.new('055a 444 285')
+    expect(my_string.valid?).to be false
+  end
+
+  it 'valid strings with a non digit added at the end become invalid' do
+    my_string = Tools::Luhn.new('059a')
+    expect(my_string.valid?).to be false
+  end
+
+  it 'valid string with punctuation included become invalid' do
+    my_string = Tools::Luhn.new('055-444-285')
+    expect(my_string.valid?).to be false
+  end
+
+  it 'valid strings with symbols included become invalid' do
+    my_string = Tools::Luhn.new('055^ 444$ 285')
+    expect(my_string.valid?).to be false
+  end
+
+  it 'single zero space is invalid' do
+    my_string = Tools::Luhn.new(' 0')
+    expect(my_string.valid?).to be false
+  end
+
+  it 'more than a single zero is valid' do
+    my_string = Tools::Luhn.new('0000 0')
+    expect(my_string.valid?).to be true
+  end
+
+  it 'input digit 9 is correctly converted to output digit 9' do
+    my_string = Tools::Luhn.new('091')
+    expect(my_string.valid?).to be true
+  end
+
+  it 'strings with non digits is invalid' do
+    my_string = Tools::Luhn.new(':9')
+    expect(my_string.valid?).to be false
   end
 
 end
